26477039e96eab4e42e4ff32b4da3fd4a3e34d34
Léo <leo.cigale@gmail.com>
2025-12-19 12:45:04 +0100

    FIX: Type-only import for RefObject

--- src/hooks/use-mouse-position-ref.tsx
+++ src/hooks/use-mouse-position-ref.tsx
@@ -1,4 +1,5 @@
-import { RefObject, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
+import type { RefObject } from "react";
 
 export const useMousePositionRef = (
   containerRef?: RefObject<HTMLElement | SVGElement>
